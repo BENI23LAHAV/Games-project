1b80172856d1b8fbfb2434a75b0d20668f3e75de
BENI23LAHAV <beni0548472300@gmail.com>
2024-06-03 14:24:16 +0300

    The important update was the previus

--- Java Script/menuScript.js
+++ Java Script/menuScript.js
@@ -3,3 +3,11 @@ document.querySelector("#logout").addEventListener("click", () => {
   // Redirect to landing page
   window.location.replace("../index.html");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let username = localStorage.getItem("current user") || "User";
+  let score = localStorage.getItem("score") || 0;
+  username = JSON.parse(username);
+  document.querySelector("#username").innerText = username.user_name;
+  document.querySelector("#score").innerText = username.score;
+});
